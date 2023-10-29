eefbf0782c5dc30dd8e3b06c9236c6d0560f33cc
leoprone <102326846+leoprone@users.noreply.github.com>
2023-10-29 18:49:47 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,36 @@
+
+
+/* ********** Menu ********** */
+
+/*función anónima autoejecutable*/
+
+
+
+((d) => {
+    const $btnMenu = d.querySelector(".menu-btn"),
+      $menu = d.querySelector(".menu");
+  
+  
+    $btnMenu.addEventListener('click', (e) => {
+      $btnMenu.firstElementChild.classList.toggle("none");
+      $btnMenu.lastElementChild.classList.toggle("none");
+      $menu.classList.toggle("is-active");
+    });
+  /*esconder el menu cuando ya se seleccionó una sección o
+  si se clickea en cualquier otro sector de la pantalla
+   (por delegación de eventos, asignando el click a un
+    elem. de nivel superior, en este caso, el document)*/ 
+  d.addEventListener("click", (e) => {
+  
+  
+    if (e.target.matches(".menu-btn svg")) return false;/*no pasa nada ahí*/
+  
+    $btnMenu.firstElementChild.classList.remove("none");
+      $btnMenu.lastElementChild.classList.add("none");
+      $menu.classList.remove("is-active");
+   
+      
+    });
+  
+  })(document);
+  
